d65b7d731835e66f54b64f59f55ba1f20ea163a7
Your Name <you@example.com>
2019-03-11 22:26:24 +0300

    Создана стартовая страница авторизации ВК

--- VKVideoSearch/ViewController.swift
+++ VKVideoSearch/ViewController.swift
@@ -7,11 +7,29 @@
 //
 
 import UIKit
+import WebKit
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
-class ViewController: UIViewController {
-
+    var loginView:WKWebView!
+    var token: String!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        loginView = WKWebView(frame: .zero, configuration: webConfiguration)
+        loginView.uiDelegate = self
+        view = loginView
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loginView.navigationDelegate = self
+        let myURL = URL(string:"https://oauth.vk.com/authorize?client_id=6895508&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=video&response_type=token&v=5.52")
+        let myRequest = URLRequest(url: myURL!)
+        loginView.load(myRequest)
+        //6895508
+        //6789060
         // Do any additional setup after loading the view, typically from a nib.
     }
 
